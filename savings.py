e691b3205dd4d5a96aa5749565dfefd459f3afd2
Michael M <mpmlearn2@gmail.com>
2024-09-17 09:48:35 -0400

    create savings.py

---
+++ savings.py
@@ -0,0 +1,7 @@
+discount = float(input("Enter the discount(in decimal form): "))
+
+original = float(input("What is the original price: "))
+savings = discount * original
+discountedPrice = original - savings 
+
+print("%.2f" % discountedPrice)
