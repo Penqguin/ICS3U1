0cd4433a495b35f882467217190a39bc8abad98c
Michael M <mpmlearn2@gmail.com>
2024-09-18 10:28:22 -0400

    create sidelengths

---
+++ sidelengths.py
@@ -0,0 +1,7 @@
+num_of_side_lengths = int(input("Input the number of side lengths: "))
+side_lengths = [int(input("input your side length: ")) for _ in range(num_of_side_lengths)]
+perimeter = 0
+
+for i in range(num_of_side_lengths):
+    perimeter += side_lengths[i]
+print("Perimeter is:", perimeter)
