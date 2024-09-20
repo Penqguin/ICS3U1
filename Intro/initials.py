d1eccc307e406951a49bc9edfb2dd811cc39f33c
Michael M <mpmlearn2@gmail.com>
2024-09-20 10:07:23 -0400

    update

---
+++ Intro/initials.py
@@ -0,0 +1,6 @@
+name = [str(input("Enter your first then your last name: ")) for i in range(2)]
+first_name = name[0] 
+last_name = name[0]
+initials = first_name[0] + last_name[0]
+
+print("Your initials are ", initials)
---
+++ Intro/phonenumber.py
@@ -0,0 +1,3 @@
+pn = str(input("Enter your phone number: "))
+formatPn = "(" + pn[0:3] + ") " + pn[3:6] + "-" + pn[6:10]
+print(formatPn)
