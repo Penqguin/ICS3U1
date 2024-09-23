cc30a0212c2133b995f6c543013c1c181caab282
Michael M <mpmlearn2@gmail.com>
2024-09-23 09:19:59 -0400

    create

---
+++ if statements/feelings.py
@@ -0,0 +1,12 @@
+feeling = str(input("How are you feeling? "))
+
+if feeling == "happy":
+    print(":)")
+elif feeling == "sad":
+    print(":(")
+elif feeling == "sobbing":
+    print(";-;")
+elif feeling == "confused":
+    print(":S")
+else:
+    print("Ok then")
