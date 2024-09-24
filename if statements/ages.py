d79da07902d59853e510687b2b962bebf57b3543
Michael M <mpmlearn2@gmail.com>
2024-09-24 10:03:17 -0400

    create

---
+++ if statements/ages.py
@@ -0,0 +1,30 @@
+a, b, c = map(int, input("Enter 3 ages? ").split())
+eldest, middle, yongest = 0, 0, 0
+if a > b and a > c:
+    eldest = a
+    if b > c:
+        yongest = c
+        middle = b
+    elif c > b:
+        yongest = b
+        middle = c
+
+if b > a and b > c:
+    eldest = b
+    if a > c:
+        yongest = c
+        middle = a
+    elif c > a:
+        yongest = a
+        middle = c
+
+if c > a and c > b:
+    eldest = c
+    if b > a:
+        yongest = a
+        middle = b
+    elif a > b:
+        yongest = b
+        middle = a
+
+print(eldest, middle, yongest)
