da39c8c85942124df369cb0d8ca25faee0201bea
Michael M <mpmlearn2@gmail.com>
2024-09-24 10:08:28 -0400

    update ages.py

--- if statements/ages.py
+++ if statements/ages.py
@@ -1,27 +1,27 @@
 a, b, c = map(int, input("Enter 3 ages? ").split())
 eldest, middle, yongest = 0, 0, 0
-if a > b and a > c:
+if a >= b and a >= c:
     eldest = a
-    if b > c:
+    if b >= c:
         yongest = c
         middle = b
-    elif c > b:
+    elif c >= b:
         yongest = b
         middle = c
-elif b > a and b > c:
+elif b >= a and b >= c:
     eldest = b
-    if a > c:
+    if a >= c:
         yongest = c
         middle = a
-    elif c > a:
+    elif c >= a:
         yongest = a
         middle = c
-elif c > a and c > b:
+elif c >= a and c >= b:
     eldest = c
-    if b > a:
+    if b >= a:
         yongest = a
         middle = b
-    elif a > b:
+    elif a >= b:
         yongest = b
         middle = a
 
