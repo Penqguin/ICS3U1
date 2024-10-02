f90a85cca219e8920f27f707fee46427c8fc48af
Michael M <mpmlearn2@gmail.com>
2024-10-02 09:51:24 -0400

    create

---
+++ Loops/infinite.py
@@ -0,0 +1,13 @@
+i = True
+while i:
+    try:
+        a = int(input("What do you want to count down from: "))
+    except ValueError or a > 0:
+        print("try again")
+    else: 
+        i = False
+for i in range(1, 11):
+    print(i)
+for i in range(a, -1, -1):
+    print(i)
+print('times up!')
