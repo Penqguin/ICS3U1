c5b3781198619541245f71a2afca7a193fb9e91d
Michael M <mpmlearn2@gmail.com>
2024-10-15 09:19:46 -0400

    update Q#4

--- Loops/while loops/Q#4.py
+++ Loops/while loops/Q#4.py
@@ -1,6 +1,6 @@
 import random
 
-ans, guess, num = random.randint(1, 21), int(input("Enter a number between 1 and 20: ")), 0
+ans, guess, num = random.randint(1, 21), int(input("Enter a number between 1 and 20: ")), 1
 
 while guess != ans:
     if guess > ans:
@@ -8,5 +8,6 @@ while guess != ans:
     elif guess < ans:
         print("Guess a higher number")
     guess = int(input("Enter a number between 1 and 20: "))
+    num += 1
         
-print(f"Great Job! You guessed the number!")
+print(f"Great Job! You guessed the number! {guess} in {num} guesses")
